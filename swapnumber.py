ca5b3c62203374e74868bea42b23fa57779b7023
Joyeeta2003 <joyeetadas597@gmail.com>
2024-09-25 17:19:38 +0530

    swapnumber.py

---
+++ swapnumber.py
@@ -0,0 +1,11 @@
+x = int(input("enter first number:"))
+y = int(input("enter second number:"))
+
+print ("Before swapping: ")
+print("Value of x : ", x, " and y : ", y)
+
+# code to swap 'x' and 'y'
+x, y = y, x
+
+print ("After swapping: ")
+print("Value of x : ", x, " and y : ", y)
