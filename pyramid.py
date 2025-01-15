3eba90ac5766152ff8165a3280be0afbc2213ad4
Joyeeta2003 <joyeetadas597@gmail.com>
2025-01-15 15:45:42 +0530

    pyramid.py

--- pyramid.py
+++ pyramid.py
@@ -1,22 +1,13 @@
-import java.io.*;
+public class Main {
 
-public class GeeksForGeeks
-{
-	public static void printStars(int n)
-	{
-		int i, j;
-		for(i=0; i<n; i++)
-		{
-			for(j=0; j<=i; j++)
-			{
-				System.out.print("* ");
-			}
-			System.out.println();
-		}
-}
-	public static void main(String args[])
-	{
-		int n = 5;
-		printStars(n);
-	}
+  public static void main(String[] args) {
+    int rows = 5;
+
+    for (int i = 1; i <= rows; ++i) {
+      for (int j = 1; j <= i; ++j) {
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
 }
